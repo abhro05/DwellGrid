d8d9ae33e81201c249a6283b82383ba98d9e1455
abhro05 <mailtoabhro02@gmail.com>
2026-06-22 00:48:34 +0530

    auth started

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const reviews = require("./routes/review.js");
 const { listingSchema, reviewSchema } = require("./schema.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
 const MONGO_URL= "mongodb://127.0.0.1:27017/DwellGrid";
 async function main() {
@@ -49,10 +52,27 @@ app.get("/", (req, res) => {
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
+});
+
+app.get("/demouser", async (req, res) => {
+    let fakeUser = new User({
+        email: "student@gmail.com",
+        username: "delta-student"
+    });
+
+    let registeredUsers = await User.register(fakeUser, "helloworld");
+    res.send(registeredUser)
 })
 
 const validateListing = (req, res, next) => {
